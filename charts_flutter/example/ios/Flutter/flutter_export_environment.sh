5b6b0d01cd2b7de32509ce53f411501e998e8a2e
Russell Soza <endosoza1@gmail.com>
2025-03-05 22:21:08 -0600

    Fix: Agregando cambios en el pubspec.yaml

--- charts_flutter/example/ios/Flutter/flutter_export_environment.sh
+++ charts_flutter/example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\endos\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\endos\Tesis\charts-partograph\charts_flutter\example"
+export "FLUTTER_ROOT=C:\Users\russe\AppData\Local\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Development\charts-partograph\charts_flutter\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
